24312221dacfdd50d7c2231ac2d2185f5b932c1a
muhlenlogan8 <muhlenlogan8@gmail.com>
2023-12-03 22:16:21 -0500

    split data and add rank functions

--- data.py
+++ data.py
@@ -18,7 +18,7 @@ def add_winner(df):
         else:
             home_pk_score, away_pk_score = 0, 0
         
-        # Scores can be added to simplify the process. This is allowed due to the pk scores being 0 if the match didn't go to penalties
+        # Scores can be added to simplify the process. This is allowed due to the pk scores being 0 if the match didn"t go to penalties
         if row["home_score"] + home_pk_score > row["away_score"] + away_pk_score:
             return row["home_team"]
         elif row["home_score"] + home_pk_score < row["away_score"] + away_pk_score:
@@ -30,16 +30,81 @@ def add_winner(df):
     return df
 
 
-# NOT WORKING CURRENTLY
+# Split matches into one big dataframe so for each game each team get their own row
 def split_matches(df):
-    home_cols = {"home_team": "Team", "away_team": "Opponent"}
-    home_df = df[["year", "home_team", "home_score", "game_score", "away_score", "pk_score", "away_team", "winner"]].rename(home_cols)
-    
-    away_cols = {"away_team": "Team", "home_team": "Opponent"}
-    away_df = df[["year", "away_team", "away_score", "game_score", "home_score", "pk_score", "home_team", "winner"]].rename(away_cols)
-    
-    combined_df = pd.concat([home_df, away_df], ignore_index = True)
-    return combined_df
+
+    # Initialize lists
+    year_list = []
+    team_list = []
+    home_away_list = []
+    home_score_list = []
+    game_score_list = []
+    opp_score_list = []
+    pk_score_list = []
+    opponent_list = []
+    winner_list = []
+
+    # Helper function to flip the game_score of originally away teams
+    def game_score_helper(score):
+        # Ensure no strange hyphens are in the score
+        score.replace("–", "-")
+
+        # Get just game score
+        game_score = score.replace("Awarded[note 1]", "").replace(" (a.e.t./g.g.)", "").replace(" (a.e.t.)", "")
+
+        # Flip game score
+        game_score = game_score[::-1]
+
+        # Add back in extra information
+        modified_score = game_score + score[3:]
+        return modified_score
+
+    # Go through each match and get the data
+    for index, row in df.iterrows():
+        # For home team
+        year_list.append(row["year"])
+        team_list.append(row["home_team"])
+        home_away_list.append("Home")
+        home_score_list.append(row["home_score"])
+        game_score_list.append(row["game_score"])
+        opp_score_list.append(row["away_score"])
+        pk_score_list.append(row["pk_score"])
+        opponent_list.append(row["away_team"])
+        winner_list.append(row["winner"])
+
+        # For away team
+        year_list.append(row["year"])
+        team_list.append(row["away_team"])
+        home_away_list.append("Away")
+        home_score_list.append(row["away_score"])
+        game_score = game_score_helper(row["game_score"])
+        game_score_list.append(game_score)
+        opp_score_list.append(row["home_score"])
+        pk_score_list.append(row["pk_score"])
+        opponent_list.append(row["home_team"])
+        winner_list.append(row["winner"])
+
+    dict_columns = {"year": year_list, "team": team_list, "home_away": home_away_list, "home_score": home_score_list, "game_score": game_score_list, "opp_score": opp_score_list, "pk_score": pk_score_list, "opponent": opponent_list, "winner": winner_list}
+    df_combined = pd.DataFrame(dict_columns)
+    return df_combined
+
+
+# Add rankings to dataframe
+def add_rankings(df_matches, df_ranks):
+
+    # Merge the two dataframes on the team column
+    df_merged = pd.merge(df_matches, df_ranks[['team', 'rank']], on='team', how='left')
+
+    # Rename the rank column
+    df_merged.rename(columns={'rank': 'team_rank'}, inplace=True)
+
+    # Fill NaN values and convert the team_rank column to integers
+    df_merged['team_rank'] = df_merged['team_rank'].fillna(0).astype(int)
+
+    # Restructure columns
+    df_result = df_merged[["year", "team", "team_rank", "home_away", "home_score", "game_score", "opp_score", "pk_score", "opponent", "winner"]]
+
+    return df_result
 
 
 def get_data():
@@ -47,7 +112,9 @@ def get_data():
     df_matches = clean_strings(df_matches)
     df_matches = add_winner(df_matches)
     df_matches = df_matches.reset_index(drop = True)
-    print(df_matches)
+    df_matches = split_matches(df_matches)
+    df_final = add_rankings(df_matches, df_ranks)
+    print(df_final)
 
 
 get_data()
